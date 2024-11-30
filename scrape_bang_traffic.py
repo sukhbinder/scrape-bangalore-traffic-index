c2761b1161ea774e4c6803a5dbb4b67f064de1ee
Sukhbinder Singh <sukhbindersingh@gmail.com>
2024-11-30 20:10:45 +0530

    Added user agent

--- scrape_bang_traffic.py
+++ scrape_bang_traffic.py
@@ -3,7 +3,10 @@ import urllib.request
 import urllib.parse
 
 url="https://trafficindex.org/chennai/"
-req = urllib.request.Request(url)
+req = urllib.request.Request(url,
+                             headers={
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+    } )
 with urllib.request.urlopen(req) as response:
         html = response.read().decode("utf-8")
 pattern = r"<tr>\s*<td.*?><a.*?href='.*?'.*?>.*?</a></td>\s*<td>(.*?)</td>\s*<td><a.*?href='.*?'>(.*?)</a></td>\s*</tr>"
